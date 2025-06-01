d8cf3255d5156bb6a6a5062d4dfa6cf508c15018
HarrisYu5 <hy002421@bu.edu>
2025-05-31 20:45:21 -0400

    added mini_fb

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    first_name = models.TextField(blank=True)
+    last_name  = models.TextField(blank=True)
+    city = models.TextField(blank=True)
+    email = models.TextField(blank=True)
+    profile_pic_url = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Profile
+
+
 
 # Create your views here.
+
+
+class ShowAllProfilesView(ListView):
+    model = Profile
+    template_name = 'mini_fb/show_all_profiles.html' 
+    context_object_name = 'profiles'
+
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/show_profile.html'
+    context_object_name = 'profile'
+
